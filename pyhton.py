46f0fe98c5e9ddb7f054c388a301c13c4f5f72b2
Asabeneh <asabeneh@gmail.com>
2022-08-31 17:20:22 +0300

    some changes

---
+++ pyhton.py
@@ -0,0 +1,46 @@
+
+print('Hello World!')
+print('Washera is the best place to learn coding')
+print('Welcome to Python Programming for Absolute Beginners')
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(2 / 3)
+print(3 % 2)
+print(2 ** 5)
+print('Python')
+print(len('Python'))
+print(round(3.14))
+input('Enter your name')
+
+first_name = 'Asabeneh'
+last_name = 'Yetayeh'
+age = 250
+country = 'Finland'
+is_married = True
+skills = ['Python', 'JavaScript','TypeScript']
+
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(2 / 3)
+print(3 % 2)
+print(7 // 3)
+print(2 ** 5)
+
+print(3 > 2)
+print(2 < 3)
+print(1 >= 0)
+
+print(3 > 2 and 2 < 3)
+print(2 < 3 or 1 >= 0)
+print(not 2 > 3)
+rect = {
+	'width':20,
+ 'length':30
+}
+def make_square(n):
+  return n ** 2
+
+
+
